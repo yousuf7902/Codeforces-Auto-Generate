c26df0e93b1f4628cad17f9edae97d3cb2685449
Yousuf Hassan <yousufhassan04@gmail.com>
2022-02-02 13:11:00 +0600

    Add solution for problem `A - Watermelon` Link: https://codeforces.com/contest/4/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/4/submission/144909005

--- codeforces/4/A.cpp
+++ codeforces/4/A.cpp
@@ -2,9 +2,18 @@
 using namespace std;
 
 int main(){
-    int w;
+    int w,sum=0,count=0;
     cin >> w;
-    if(w%2==0 && w!=2){
+
+    for (int i = 2; i <= w;i+=2){
+        for (int j = 2; j <= w;j+=2){
+            sum = i + j;
+            if(sum==w){
+                count++;
+            }
+        }
+    }
+    if(count>0){
         cout << "YES";
     }
     else{
