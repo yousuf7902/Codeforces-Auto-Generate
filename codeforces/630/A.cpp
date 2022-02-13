162562417912871c247013466c87d0be86a41147
Yousuf Hassan <yousufhassan04@gmail.com>
2022-02-13 23:13:00 +0600

    Add solution for problem `A - Again Twenty Five!` Link: https://codeforces.com/contest/630/problem/A Tags: number theory, *800 Ref: https://codeforces.com/contest/630/submission/146262563

--- codeforces/630/A.cpp
+++ codeforces/630/A.cpp
@@ -4,9 +4,10 @@ using namespace std;
 int main()
 {
      long long int n;
-     int fixed = 25;
+     int fixed = 5;
      cin >> n;
      if(n>1){
+          fixed = fixed * fixed;
           cout << fixed;
      }
 } 
