1524f77c6d03601c1cbe58965e4e5aacfeaba87d
Yousuf Hassan <yousufhassan04@gmail.com>
2022-02-14 22:32:00 +0600

    Add solution for problem `A - Petya and Strings` Link: https://codeforces.com/contest/112/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/112/submission/146427012

--- codeforces/112/A.cpp
+++ codeforces/112/A.cpp
@@ -1,22 +1,19 @@
-    #include<bits/stdc++.h>
-    using namespace std;
-     
-    int main(){
-        int count = 0;
-        string n1,n2;
-        cin >> n1>>n2;
-        for (int i = 0; i < n1.size();i++){
-            if(tolower(n1[i])<tolower(n2[i])){
-                count = -1;
-                cout << count << endl;
-                return 0;
-            }
-            else if(tolower(n1[i])>tolower(n2[i])){
-                count = 1;
-                cout << count << endl;
-                return 0;
-            }
-        }
-        cout << count;
-        return 0;
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int count = 0;
+    string n1,n2;
+    cin >> n1>>n2;
+    for (int i = 0; i < n1.size();i++){
+        n1[i] = tolower(n1[i]);
+        n2[i] = tolower(n2[i]);
     }
+    if(n1<n2){
+        count = -1;
+    }
+    else if(n1>n2){
+        count = 1;
+    }
+    cout << count;
+}
