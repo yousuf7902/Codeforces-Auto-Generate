bdeb1fd217d5aa72a120cc7b962ebe9a69befe78
Yousuf Hassan <yousufhassan04@gmail.com>
2022-03-05 11:46:00 +0600

    Add solution for problem `A - Expression` Link: https://codeforces.com/contest/479/problem/A Tags: brute force, math, *1000 Ref: https://codeforces.com/contest/479/submission/148422872

--- codeforces/479/A.cpp
+++ codeforces/479/A.cpp
@@ -3,16 +3,19 @@ using namespace std;
 
 int main()
 {
-    int a, b, c, maxNum;
+    int a, b, c, maxNum = INT_MIN, result;
     cin >> a >> b >> c;
 
-    maxNum = a * b * c;
-    maxNum = max(maxNum, a+b*c);
-    maxNum = max(maxNum, a*(b+c));
-    maxNum = max(maxNum, a*b+c);
-    maxNum = max(maxNum, (a+b)*c);
-    maxNum = max(maxNum, a+b+c);
-
+    result = a + b * c;
+    maxNum = max(maxNum, result);
+    result = a * (b + c);
+    maxNum = max(maxNum, result);
+    result = a * b * c;
+    maxNum = max(maxNum, result);
+    result = (a+b)*c;
+    maxNum = max(maxNum, result);
+    result = (a+b+c);
+    maxNum = max(maxNum, result);
     cout << maxNum << endl;
     return 0;
 }
