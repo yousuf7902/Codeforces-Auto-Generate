d78aeeef10eda8a3ed203c90fcccecb100d26afc
Yousuf Hassan <yousufhassan04@gmail.com>
2022-03-12 13:04:00 +0600

    Add solution for problem `A - Madoka and Math Dad` Link: https://codeforces.com/contest/1647/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1647/submission/149389135

--- codeforces/1647/A.cpp
+++ codeforces/1647/A.cpp
@@ -1,39 +1,77 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void check(){
-    int num;
-    cin >> num;
-
-    bool f;
-    if((num-1)%3==0){
-        f = true;
-    }
-    else{
-        f = false;
-    }
-
-    while(num!=0){
-        if(f){
-            cout << 1;
-            num--;
-            f = false;
-        }
-        else{
-            cout << 2;
-            num -= 2;
-            f = true;
-        }
-    }
-    cout << endl;
-}
-
-int main(){
+int main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
-
     int t;
-    cin >> t; while(t--)
+    cin >> t;
 
-    check();
+    while (t--)
+    {
+        int num, sum = 0;
+        cin >> num;
+        if (num == 1 || num == 2)
+        {
+            cout << num;
+        }
+        else
+        {
+            if (num % 3 == 0)
+            {
+                while(1){
+                    int i = 2;
+                    sum = sum + i;
+                    cout << i;
+                    if(sum>=num){
+                        break;
+                    }
+                    i--;
+                    sum = sum + i;
+                    cout << i;
+                    if(sum>=num){
+                        break;
+                    }
+                }
+            }
+            else if (num % 3 == 1)
+            {
+                while(1){
+                    int i = 1;
+                    sum = sum + i;
+                    cout << i;
+                    if(sum>=num){
+                        break;
+                    }
+                    i++;
+                    sum = sum + i;
+                    cout << i;
+                    if(sum>=num){
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                while(1){
+                    int i = 2;
+                    sum = sum + i;
+                    cout << i;
+                    if(sum>=num){
+                        break;
+                    }
+                    i--;
+                    sum = sum + i;
+                    cout << i;
+                    if(sum>=num){
+                        break;
+                    }
+                }
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
