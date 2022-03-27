fdcb6c4a11ec2e25ccde3b522014642d37810615
Yousuf Hassan <yousufhassan04@gmail.com>
2022-03-27 23:05:00 +0600

    Add solution for problem `A - Marin and Photoshoot` Link: https://codeforces.com/contest/1658/problem/A Tags: constructive algorithms, implementation, math, *800 Ref: https://codeforces.com/contest/1658/submission/151168935

--- codeforces/1658/A.cpp
+++ codeforces/1658/A.cpp
@@ -14,15 +14,15 @@ int main()
         string s;
         cin >> s;
 
-        for (int i = 0; i < n - 1;i++){
+        for (int i = 0; i <n-1;i++){
             if(s[i]=='0' && s[i+1]=='0'){
-                ans += 2;
+                    ans += 2;
             }
-            else if(s[i]=='0' && s[i+1]=='1' && s[i+2]=='0'){
+            if(s[i]=='0' && s[i+1]=='1' && s[i+2]=='0'){
                 ans++;
             }
         }
-            cout << ans << endl;
+        cout << ans << endl;
     }
     return 0;
 }
