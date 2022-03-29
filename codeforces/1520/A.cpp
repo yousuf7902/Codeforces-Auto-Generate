257daad23331d7d829382cb25d81c5c35eeb13a9
Yousuf Hassan <yousufhassan04@gmail.com>
2022-03-29 16:09:00 +0600

    Add solution for problem `A - Do Not Be Distracted!` Link: https://codeforces.com/contest/1520/problem/A Tags: brute force, implementation, *800 Ref: https://codeforces.com/contest/1520/submission/151317600

---
+++ codeforces/1520/A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n,i=0,flag=0;
+        string s;
+
+        cin>>n>>s;
+
+        while(i<n){
+
+            if(s[i]!=s[i+1]){
+                for(int j=i+1;j<n;j++){
+                    if(s[i]==s[j]){
+                        flag=1;
+                        break;
+                    }
+                }
+            }
+            i++;
+        }
+        if(flag>0){
+            cout<<"NO"<<endl;
+        }
+        else{
+            cout<<"YES"<<endl;
+        }
+    }
+
+    return 0;
+}
