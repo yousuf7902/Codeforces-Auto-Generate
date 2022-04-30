fa73016cbba83ecc41f5ed9943c4947c07baa2cf
Yousuf Hassan <yousufhassan04@gmail.com>
2022-04-30 14:23:00 +0600

    Add solution for problem `A - Sereja and Dima` Link: https://codeforces.com/contest/381/problem/A Tags: greedy, implementation, two pointers, *800 Ref: https://codeforces.com/contest/381/submission/155366613

--- codeforces/381/A.cpp
+++ codeforces/381/A.cpp
@@ -5,37 +5,48 @@ int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int c, flag = 0, s = 0, d = 0;
+    int c, flag = 0, maxNum = 0, s = 0, d = 0;
     cin >> c;
+    int arr[c];
 
-    vector<int> vec (c);
-    for (int i = 0; i < c;i++)
-        cin >> vec[i];
+    vector<int> s_vec;
+    vector<int> d_vec;
 
-    while(!vec.empty()){
-        if(flag==0){
-            if(*vec.begin()>vec.back()){
-                s += *vec.begin();
-                vec.erase(vec.begin());
-            }
-            else{
-                s += vec.back();
-                vec.pop_back();
-            }
-            flag =1;
+    for (int i = 0; i < c; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0, j = c - 1; j >= 0; j--)
+    {
+        if (arr[i] <= arr[j])
+        {
+            maxNum = arr[j];
+            arr[j] = 0;
         }
-        else{
-            if(*vec.begin()>vec.back()){
-                d += *vec.begin();
-                vec.erase(vec.begin());
-            }
-            else{
-                d += vec.back();
-                vec.pop_back();
-            }
+        else
+        {
+            maxNum = arr[i];
+            arr[i] = 0;
+            i++;
+            j++;
+        }
+
+        if (flag == 0)
+        {
+            s_vec.emplace_back(maxNum);
+            flag = 1;
+        }
+        else
+        {
+            d_vec.emplace_back(maxNum);
             flag = 0;
         }
     }
+    s = accumulate(s_vec.begin(), s_vec.end(), 0);
+    d = accumulate(d_vec.begin(), d_vec.end(), 0);
+
     cout << s << " " << d << endl;
+
     return 0;
 }
