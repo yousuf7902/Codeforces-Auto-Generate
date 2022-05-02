82b9098c24674efd16dfd82037dcbac3ee59a823
Yousuf Hassan <yousufhassan04@gmail.com>
2022-05-02 13:13:00 +0600

    Add solution for problem `A - Anton and Letters` Link: https://codeforces.com/contest/443/problem/A Tags: constructive algorithms, implementation, *800 Ref: https://codeforces.com/contest/443/submission/155563113

--- codeforces/443/A.cpp
+++ codeforces/443/A.cpp
@@ -6,23 +6,20 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    //Another way
+    set<char> s;
 
-    string s1, s2=" ";
-    getline(cin, s1);
-
-    for (int i = 0; i < s1.size();i++){
-        if(s1[i]!='{' && s1[i]!='}' && s1[i]!=' ' &&s1[i]!=','){
-            s2=s2+s1[i];
+    for (int i = 0;;i++){
+        char c;
+        cin >> c;
+        s.insert(c);
+        if(c=='}'){
+            break;
         }
     }
-    sort(s2.begin(), s2.end());
-    int count = 0;
-    for (int i = 0; i < s2.size() - 1;i++){
-        if(s2[i]!=s2[i+1]){
-            count++;
-        }
-    }
-    cout << count << endl;
+
+    if(s.size()>3) cout << s.size()-3 << endl;
+    else
+        cout << s.size() - 2 << endl;
+
     return 0;
 }
