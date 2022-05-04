5a87fdebf71166b9a76b1c66b608df095f21585d
Yousuf Hassan <yousufhassan04@gmail.com>
2022-05-04 12:28:00 +0600

    Add solution for problem `A - I Wanna Be the Guy` Link: https://codeforces.com/contest/469/problem/A Tags: greedy, implementation, *800 Ref: https://codeforces.com/contest/469/submission/155817526

--- codeforces/469/A.cpp
+++ codeforces/469/A.cpp
@@ -5,47 +5,31 @@ int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
-    int n, p, q, count = 1;
+
+    int n,p ,q;
     cin >> n;
-
-    vector<int> vec;
+    set<int> st;
     cin >> p;
-    for (int i = 0; i < p; i++)
-    {
-        int x;
-        cin >> x;
-        vec.emplace_back(x);
+    int arr1[p],max1=INT_MIN ;
+
+    for (int i = 0; i < p;i++){
+        cin >> arr1[i];
+        st.insert(arr1[i]);
     }
+
     cin >> q;
-    for (int i = 0; i < q; i++)
-    {
-        int x;
-        cin >> x;
-        vec.emplace_back(x);
+    int arr2[q], max2 = INT_MIN;
+    for (int i = 0; i < q;i++){
+        cin >> arr2[i];
+        st.insert(arr2[i]);
     }
 
-    if(p==0 && q==0){
-        cout << "Oh, my keyboard!" << endl;
-        return 0;
-    }
-    
-    sort(vec.begin(), vec.end());
-    for (int i = 0; i < vec.size() - 1; i++)
-    {
-
-        if (vec[i] != vec[i + 1])
-        {
-            count++;
-        }
-    }
-    if (count == n)
-    {
+    if(st.size()==n){
         cout << "I become the guy." << endl;
     }
-    else
-    {
+    else{
         cout << "Oh, my keyboard!" << endl;
-    }
+    } 
+
     return 0;
 }
