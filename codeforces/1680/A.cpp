c9e07e8bea52240aefb0c25b123206b1cae40ec9
Yousuf Hassan <yousufhassan04@gmail.com>
2022-05-14 14:38:00 +0600

    Add solution for problem `A - Minimums and Maximums` Link: https://codeforces.com/contest/1680/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/1680/submission/157132376

--- codeforces/1680/A.cpp
+++ codeforces/1680/A.cpp
@@ -1,33 +1,25 @@
-#include<bits/stdc++.h>
-    using namespace std;
-     
-    int main(){
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        int t;
-        cin >> t;
-     
-        while(t--){
-            int l1, l2, r1, r2;
-            cin >> l1 >> r1 >> l2 >> r2;
-     
-            if(l1<=l2){
-                if(l2<=r1){
-                    cout << l2<< endl;
-                }
-                else{
-                    cout << l2+l1<< endl;
-                }
-            }
-            else{
-                if(l1>r2){
-                    cout << l1 +l2 << endl;
-                }
-                else{
-                    cout << l1 << endl;
-                }
-                
-            }
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int l1, r1, l2, r2;
+        cin>>l1>>r1>>l2>>r2;
+        
+        if(l1>l2){
+            swap(l1, l2);
+            swap(r1,r2);
         }
-        return 0;
+        
+        if(l2<=r1){
+            cout<<l2<<endl;
+        }
+        else{
+            cout<<l1+l2<<endl;
+        }
     }
+    return 0;
+}
