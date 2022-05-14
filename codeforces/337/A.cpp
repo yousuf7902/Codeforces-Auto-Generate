e3c064ad572beb265a805816526f32f2f89f5ecd
Yousuf Hassan <yousufhassan04@gmail.com>
2022-05-15 00:22:00 +0600

    Add solution for problem `A - Puzzles` Link: https://codeforces.com/contest/337/problem/A Tags: greedy, *900 Ref: https://codeforces.com/contest/337/submission/157238535

--- codeforces/337/A.cpp
+++ codeforces/337/A.cpp
@@ -5,19 +5,18 @@ int main()
 {
     int n, m;
     cin >> n >> m;
-
-    int arr[m];
+ 
+    int arr[m],minNum=INT_MAX,diff;
 
     for (int i = 0; i < m;i++){
         cin >> arr[i];
     }
-
     sort(arr, arr + m);
-    int ans = INT_MAX;
-    for (int i = 0; i <= m - n;i++){
-        ans = min(ans, arr[i + n - 1] - arr[i]);
+    minNum = arr[n-1] - arr[0];
+    for (int i = n; i <= m;i++){
+        diff = abs(arr[i - 1] - arr[i - n]);
+        minNum = min(minNum, diff);
     }
-
-    cout << abs(ans) << endl;
+    cout << minNum << endl;
     return 0;
 }
