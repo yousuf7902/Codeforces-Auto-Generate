e3a1cd673a631a12deb56b9a5b38b1e613ac2754
Yousuf Hassan <yousufhassan04@gmail.com>
2022-06-26 22:21:00 +0600

    Add solution for problem `A - Pangram` Link: https://codeforces.com/contest/520/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/520/submission/161878986

--- codeforces/520/A.cpp
+++ codeforces/520/A.cpp
@@ -4,25 +4,30 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int n,flag=0;
+ 
+    int n,count=0;
     cin >> n;
  
     string s;
     cin >> s;
  
-    set<char> ch;
+    for (int i = 0; i < n;i++){
+        s[i] = tolower(s[i]);
+    }
+ 
+    sort(s.begin(), s.end());
  
     for (int i = 0; i < n;i++){
- 
-        if(tolower(s[i]) >= 'a' && tolower(s[i]) <= 'z'){
-            ch.emplace(tolower(s[i]));
+        if(s[i]!=s[i+1]){
+            count++;
         }
     }
-    if(ch.size()==26){
+ 
+    if(count==26){
         cout << "YES" << endl;
     }
     else{
         cout << "NO" << endl;
     }
-    return 0;
+        return 0;
 }
