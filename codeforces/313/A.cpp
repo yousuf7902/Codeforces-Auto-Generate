01dbe0fcc6b34b6f55de3ff3dfcc2974f3666362
Yousuf Hassan <yousufhassan04@gmail.com>
2022-07-15 16:28:00 +0600

    Add solution for problem `A - Ilya and Bank Account` Link: https://codeforces.com/contest/313/problem/A Tags: implementation, number theory, *900 Ref: https://codeforces.com/contest/313/submission/164245342

---
+++ codeforces/313/A.cpp
@@ -0,0 +1,46 @@
+    //~"~"~"~"~"~"~"~"~"~"~"~"~  ♥ B I S M I L L A H I R  R A H M A N I R  R A H I M ♥   ~"~"~"~"~"~"~"~"~"~"~"~"~//  
+     
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+    // ##                                                                         ## //
+    // ##          Yousuf Hassan || CSE,IUBAT || yousufhassan04@gmail.com         ## //
+    // ##                                                                         ## //
+    // ##                        CF Handle : Yousuf7902                           ## //
+    // ##                                                                         ## //
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+     
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    //===============================================================================
+    //
+    #define         FAST ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+    #define         vc vector
+    #define         pair pair<int, int>
+    #define         st set<int>
+    #define         ll long long
+    #define         eb emplace_back
+    #define         prec(n) fixed<<setprecision(n)
+    #define         py      cout << "Yes\n";
+    #define         pn      cout << "No\n";
+    #define			endl     "\n"
+    #define         rn return 0;
+    //
+    //===============================================================================
+     
+    int main(){
+        FAST
+        
+        int n;
+        cin>>n;
+        
+        if(n>=0)cout<<n<<endl;
+        else{
+            int a,b;
+            a=n/10;
+            b=(n/100)*10+n%10;
+            cout<<max(a,b)<<endl;
+        }
+        
+        rn;
+    }
+
